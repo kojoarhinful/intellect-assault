ebf19a95379691690a14494d5932d8dd9c93e171
Bismark <bismarkpng@gmail.com>
2025-03-26 05:48:05 +0000

    fix the import

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,6 +1,6 @@
-import * as React from "react";
-import { cva } from "class-variance-authority";
-import { cn } from "@/lib/utils";
+import * as React from "react"
+import { cva } from "class-variance-authority"
+import { cn } from "../../lib/utils"
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
@@ -26,13 +26,13 @@ const buttonVariants = cva(
       size: "default",
     },
   }
-);
+)
 
 const Button = React.forwardRef<HTMLButtonElement, React.ButtonHTMLAttributes<HTMLButtonElement>>(
   ({ className, variant, size, ...props }, ref) => {
-    return <button className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />;
+    return <button className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
   }
-);
-Button.displayName = "Button";
+)
+Button.displayName = "Button"
 
-export { Button, buttonVariants };
+export { Button, buttonVariants }
